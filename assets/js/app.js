36fc84435771fd09aaa5aa3c16aa85ea72c32e68
caesarals <slac_@hotmail.com>
2023-04-13 16:42:35 -0400

    Proyecto terminado ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,13 @@
 
-console.log('Hola Mundo')
+$(document).on("dblclick", "h3.green-query", function(){
+    $(this).css(
+        "color","red"
+    )
+})
+
+
+setTimeout(() => {
+    $(document).on("click",  function(){
+        $("#quienes-somos,p").toggleClass("color-text-blue  ")
+    })
+}, 5000);
